21217709cd893bee0aeaad555e5295534e15380a
Joshua Lim <limjos@flip1.engr.oregonstate.edu>
2022-05-20 23:16:30 -0700

    d

--- server/server.js
+++ server/server.js
@@ -1,26 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3012;
 const mysql = require('mysql');
 const path = require('path');
 
-// app.use(express.static('public'));
 
-// var con = mysql.createConnection({
-//     host: "localhost",
-//     user: "123",
-//     password: "123"
-//   });
 
-// con.connect(function(err){
-//     if(err) console.log(err);
-//     console.log("sql connected");
-// })
+
+var con = mysql.createConnection({
+    host: "classmysql.engr.oregonstate.edu",
+    user: "cs340_limjos",
+    password: "6259",
+    database: "cs340_limjos"
+  });
+
+con.connect(function(err){
+    if(err) console.log(err);
+    console.log("sql connected");
+})
 
 app.get('/posts', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
     console.log("Grabbing posts table");
+
+    con.query("SELECT id, date, text, user_id FROM post", function(err, results, fields){
+        if(err) console.log(err);
+        //res.json(results);
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
 })
+
+
+
 app.get('/hashtags', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/hashtag2.html'));
     console.log("Grabbing hashtags table");
@@ -33,7 +43,10 @@ app.get('*', (req, res) => {
     console.log('redirecting');
     res.redirect('/posts');
 })
-app.listen(port, () => {
+
+
+ app.use(express.static('public'))
+
+ app.listen(port, () => {
     console.log(`App listening on port ${port}`);
  })
-
