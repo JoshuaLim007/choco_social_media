6a150ce09f5fe5328711f529e9f656d0398482be
Kyu Sung Kwon <ryanksk@gmail.com>
2022-05-26 02:11:03 -0700

    page updates

--- public/insert_data_functions.js
+++ public/insert_data_functions.js
@@ -5,23 +5,35 @@
 export function InsertPost(id, name, text, date, likes){
 	var content_section = document.getElementsByClassName("content");
 	var post_div = document.createElement("div");
-	post_div.setAttribute("id", "post");
-	post_div.setAttribute("class", "post");
 	var table = document.createElement("table");
 
 	var tr_1 = document.createElement("tr");
 	var tr_2 = document.createElement("tr");
 	var th_name = document.createElement("th");
+	var th_name_link = document.createAttribute("a");
 	var th_id = document.createElement("th");
 	var td_text = document.createElement("td");
 	var td_date = document.createElement("td");
 	var td_likes = document.createElement("td");
+	var td_likes_link = document.createElement("a");
+
+	post_div.setAttribute("id", "post");
+	post_div.setAttribute("class", "post");
+	//th_name_link.setAttribute("class", "");
+	td_likes_link.setAttribute("class", "post_td_likes_link");
+
+	th_name_link.href="/"; //insert followers page
+	td_likes_link.href = "/";//insert likes page
+
 	th_name.textContent = (name);
 	th_id.textContent = (id);
 	td_text.textContent = (text);
 	td_date.textContent  =(date);
-	td_likes.textContent  =(likes);
+	td_likes_link.textContent  =(likes);
+	td_likes.textContent = ("# of likes: ");
 
+	//th_name.appendChild(th_name_link);
+	td_likes.appendChild(td_likes_link);
 	tr_1.appendChild(th_name);
 	tr_1.appendChild(th_id);
 	tr_2.appendChild(td_text);
@@ -30,7 +42,23 @@ export function InsertPost(id, name, text, date, likes){
 	table.appendChild(tr_1);
 	table.appendChild(tr_2);
 	post_div.appendChild(table);
+	console.log(table);
 	content_section[0].appendChild(post_div);
+}
+
+export function InsertHashtag(id, name){
+	const table = document.getElementById("hashtag_table");
+	const tr = document.createElement("tr");
+	
+	var td_name = document.createElement("td");
+	var td_id = document.createElement("td");
+	
+	td_name.textContent = (name);
+	td_id.textContent = (id);
+
+	tr.appendChild(td_name);
+	tr.appendChild(td_id);
+	table.appendChild(tr);
 }
 
 export function InsertUser(id, name, email, password){
@@ -51,20 +79,5 @@ export function InsertUser(id, name, email, password){
 	tr.appendChild(td_name);
 	tr.appendChild(td_email);
 	tr.appendChild(td_password);
-	table.appendChild(tr);
-}
-
-export function InsertHashtag(id, name){
-	const table = document.getElementById("hashtag_table");
-	const tr = document.createElement("tr");
-	
-	var td_name = document.createElement("td");
-	var td_id = document.createElement("td");
-	
-	td_name.textContent = (name);
-	td_id.textContent = (id);
-
-	tr.appendChild(td_name);
-	tr.appendChild(td_id);
 	table.appendChild(tr);
 }
